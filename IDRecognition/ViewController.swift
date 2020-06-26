1f210de2f34841b8ce41cfad44713eeefba28b07
Viennarz Curtiz <viennarzc@gmail.com>
2020-06-26 20:59:50 +0800

    Initial Commit

---
+++ IDRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IDRecognition
+//
+//  Created by SCI-Viennarz on 6/26/20.
+//  Copyright © 2020 VVC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
