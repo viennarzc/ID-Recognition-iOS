69079be709154cf23a1f96286706fe0bb34c3c07
Viennarz Curtiz <viennarzc@gmail.com>
2020-07-02 12:27:04 +0800

    Rename ViewController to LiveCaptureViewController, Create MainViewController, Create CameraPhoto View Controller

---
+++ IDRecognition/CameraPhotoViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CameraPhotoViewController.swift
+//  IDRecognition
+//
+//  Created by SCI-Viennarz on 7/2/20.
+//  Copyright © 2020 VVC. All rights reserved.
+//
+
+import UIKit
+
+class CameraPhotoViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    
+  }
+  
+}
---
+++ IDRecognition/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  IDRecognition
+//
+//  Created by SCI-Viennarz on 7/2/20.
+//  Copyright © 2020 VVC. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
