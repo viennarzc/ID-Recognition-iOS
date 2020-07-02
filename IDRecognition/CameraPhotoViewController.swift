a96b17267fb52ec22cb7fb116860f535daab43a5
Viennarz Curtiz <viennarzc@gmail.com>
2020-07-02 12:37:27 +0800

    Setup Image Picker, perform segue when tapping Image Picker Flow

--- IDRecognition/CameraPhotoViewController.swift
+++ IDRecognition/CameraPhotoViewController.swift
@@ -11,11 +11,38 @@ import UIKit
 class CameraPhotoViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    setupImagePicker()
   }
   
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     
+  }
+  
+  func setupImagePicker() {
+    let imagePicker = UIImagePickerController()
+    imagePicker.delegate = self
+    
+    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+      imagePicker.sourceType = .camera
+    } else {
+      imagePicker.sourceType = .savedPhotosAlbum
+    }
+    
+    self.present(imagePicker, animated: true, completion: nil)
+  }
+  
+}
+
+extension CameraPhotoViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true) {
+      self.navigationController?.popViewController(animated: true)
+    }
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
     
   }
   
